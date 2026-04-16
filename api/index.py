34620670ace2a7cec307cc4c40f0e04577cf1535
Chukwudalu obiora <obiorachukwudalu@gmail.com>
2026-04-16 03:02:03 -0700

    HNG0

--- api/index.py
+++ api/index.py
@@ -1,11 +1,14 @@
 import os
 import sys
 
-# Add your project path
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'gender_api'))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Add BOTH root and project folder
+sys.path.append(BASE_DIR)
+sys.path.append(os.path.join(BASE_DIR, "gender_api"))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gender_api.settings")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gender_api.settings')
 
 app = get_wsgi_application()
