210f33952070a8e8517e31bcf38cb9776371f9f3
Chukwudalu obiora <obiorachukwudalu@gmail.com>
2026-04-16 01:45:33 -0700

    deployment issue

--- api/index.py
+++ api/index.py
@@ -1,4 +1,9 @@
 import os
+import sys
+
+# Add your project path
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'gender_api'))
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gender_api.settings')
